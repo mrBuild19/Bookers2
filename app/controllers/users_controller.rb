a094d1b6600e81c6c1c61c3f4c330b7c2ac8cabe
阿武大輔 <daisuke@MacBook-Air.local>
2020-04-21 11:36:45 +0900

    [Add]Bookページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,12 @@ class UsersController < ApplicationController
   def index
   	@user = current_user
   	@users = User.all
+  	@book = Book.new
   end
 
   def show
   	@user = User.find(params[:id])
+  	@book = Book.new
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   root 'home#top'
   get '/home/about' => 'home#about'
   resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
 end
