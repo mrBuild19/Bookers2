ef6bc04c3bc8a4e6737a7dd97f82dd905e1cc9c5
阿武大輔 <daisuke@MacBook-Air.local>
2020-04-22 23:28:43 +0900

    [Update]rspecによるURL直打ち対策

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :correct_user, only: [:edit]
 
   def index
   	@user = current_user
@@ -28,6 +29,13 @@ class UsersController < ApplicationController
   end
 
   private
+  def correct_user
+    user = User.find(params[:id])
+    if current_user.id != user.id
+      redirect_to user_path(current_user.id)
+    end
+  end
+
   def user_params
   	params.require(:user).permit(:name, :profile_image, :introduction)
   end
